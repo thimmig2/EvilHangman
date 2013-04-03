ffeec3a118171f02fc29c65ba8a558fbdf85fb69
thimmig2 <thimmig.2@osu.edu>
2013-04-03 17:54:22 -0400

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvilHangman2::Application.config.secret_token = '4734a1e612784023fa8270a0ffd660cc34d32a630c6941d7687d38b660ae6906d7ff0968b7f03fe4a1c397cb705f9ff2da7a6c18902c255a1e83d6d64ff0a41c'
