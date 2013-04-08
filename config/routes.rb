3973df8377a7c24ae478aa5981ef4a0b4144258d
thimmig2 <thimmig.2@osu.edu>
2013-04-08 12:22:10 -0400

    Added User Support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 EvilHangman2::Application.routes.draw do
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20130408154104_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string :username
+      t.text :password
+      t.string :profile_image_url
+
+      t.timestamps
+    end
+  end
+  def self.down
+    drop_table :users
+  end
+end
