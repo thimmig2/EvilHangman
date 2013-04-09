a591b11be764694e0779e07ba761d3774dd2972b
thimmig2 <thimmig.2@osu.edu>
2013-04-09 00:35:54 -0400

    Modified game code

--- app/models/hangman.rb
+++ app/models/hangman.rb
@@ -1,36 +1,92 @@
 
+=begin
+
+  guess a letter
+
+  there is a word family for each location of that letter in the word
+
+  along each step
+    eliminate unreachable words
+    words with double letters count as less than 1 word
+      (easier to complete the word)
+      every occurrence of a double letter reduces word value
+    words with commonly guessed letters by the player count as less than 1 word
+
+  word family is chosen by number of words in family
+
+
+Save for later :
+"abc".split.each_with_index do |letter, index|
+  puts
+end
+
+=end
+
+
 class Hangman
 
-  @@dictionaryFileName
-  @@dictionary = Array.new
+  @@dictionaryFileName = "dictionary"
+  @@LENGTH_RANGE = 5..15 # 23 is the longest word in dictionary
+
+  attr_reader :wordLength
+  attr_reader :guessedLetters
+  attr_reader :wordClass
+
+  def initialize(numberOfGuesses = 26)
+    @dictionary
+    @wordLength = -1
+
+    @guessedLetters   # array of letters that have been guessed
+    @wordClass      # array length @@wordlength of nil until player narrows dictionary down to 1
+  end
 
 
+  def guessLetter(letter)
 
-  @@wordLength
-  @@guessedLetters
+  end
 
+  # Finds most unlikely words to be guessed based on letter
+  def findEvilWords(letter)
+    evilWords = Array.new
+
+    evilWords
+  end
+
+  def printWordsLeft
+    puts @dictionary.to_s
+    @dictionary.to_s
+  end
 
   def startNewGame
-    @@wordLength = rand()
+    oldLength = @wordLength
+    @wordLength = rand(@@LENGTH_RANGE)
 
+    unless oldLength == @wordLength and @dictionary.length > 0
+      initializeDictionary
+    end
 
-
-
+    @guessedLetters = Array.new(0)
+    @wordClass = Array.new(0)
+    @wordClass[wordLength - 1] = nil
   end
 
   def initializeDictionary()
+    @dictionary = Array.new
 
-    dictIn = File.open("dictionary", "r")
+    dictIn = File.open(@@dictionaryFileName, "r")
 
     dictIn.each_line do |line|
-      if line.length == @@wordLength
-        @@dictionary.push line.strip
+      if line.length == @wordLength
+        @dictionary.push line.strip
       end
     end
 
-    dictOut.close()
-
+    dictIn.close()
   end
 
-
 end
+
+
+game = Hangman.new
+game.startNewGame
+game.printWordsLeft
